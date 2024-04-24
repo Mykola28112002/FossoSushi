c0cf2d7f700d68fe92ad7de39f3add5318048f7d
Микола Кондратюк <95100219+Mykola28112002@users.noreply.github.com>
2024-04-24 18:20:32 +0200

    1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { GlobalStateProvider } from './GlobalStateContext/GlobalStateContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Sushi-Edmonton'>
+    <BrowserRouter basename='FossoSushi'>
       {/* <Provider store={store}>
           <ThemeProvider theme={theme}> */}
       <GlobalStateProvider>
