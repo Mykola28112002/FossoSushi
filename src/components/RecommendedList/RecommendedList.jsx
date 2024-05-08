9f237fb7829f1fb566a7b3c341a9ef5109f1b5d5
Микола Кондратюк <95100219+Mykola28112002@users.noreply.github.com>
2024-05-08 13:25:23 +0200

    1

--- src/components/RecommendedList/RecommendedList.jsx
+++ src/components/RecommendedList/RecommendedList.jsx
@@ -8,13 +8,14 @@ const RecommendedList = ({ recomList }) => {
     const handleAddOrder = (e) => {
         setGlobalState(prevState => ({
             ...prevState,
-            orderArr: [...recomList, {...e, quantity: 1 }]
+            orderArr: [...prevState.orderArr, {...e, quantity: 1 }]
         }));
+        
     };
     return (
         <div className={css.setsList}>
             <ul className={css.setsUl}>
-                {recomList.length > 0 ? recomList.map((a, index) => {
+                {recomList.length > 0 ? recomList.map((a) => {
                     const { id, name, price, description, img } = a;
                     return <li key={id} className={css.item}>
                         {img !== undefined ?
@@ -35,13 +36,10 @@ const RecommendedList = ({ recomList }) => {
                                 <p style={{ whiteSpace: 'pre-wrap' }}>{description}</p>
                             </div>
                             <div className={css.btnBox}>
-                                <button className={css.btn}>
-                                    <Majesticons fill='#fff' />
-                                </button>
                                 <button
-                                    className={css.btnBy}
+                                    className={css.btn}
                                     onClick={() => handleAddOrder(a)}
-                                    disabled={recomList.some(item => item.id === a.id)}
+                                    disabled={globalState.orderArr.some(item => item.id === a.id)}
                                 >
                                     <Majesticons fill='#000' />
                                 </button>
